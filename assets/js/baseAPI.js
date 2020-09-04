18e927d68f5dac1d2a8889a71ab2170ed49f29a4
yuanpengcheng <1124187319@qq.com>
2020-09-04 17:07:47 +0800

    '完成了主页功能的开发'

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-03 15:24:44
+ * @LastEditTime: 2020-09-04 17:04:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \big\assets\js\baseAPI.js
+ */
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+  //统一为有权限的接口，设置headers请求头
+  if (options.url.indexOf('/my') != -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  //全局挂载complete回调函数
+  options.complete = function (res) {
+    //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      //强制清空token
+      localStorage.removeItem('token')
+      //强制跳转到登录页面
+      location.href = '/login.html'
+    }
+  }
 })
